d7ab1e64b6f238aabb84d23dcea8542157233768
Kamen Yovchevski <kamen.yovchevski@traverseresearch.nl>
2022-04-14 12:56:32 +0200

    Fix build errors for unsupported target OS

--- build.rs
+++ build.rs
@@ -1,11 +1,24 @@
 #[cfg(feature = "ispc_cmp")]
-use ispc::{TargetISA, TargetOS};
+fn compile_bindings() {
+    use ispc::{TargetISA, TargetOS};
 
-fn main() {
     // Compile our ISPC library, this call will exit with EXIT_FAILURE if
-    // compilation fails.
-    #[cfg(feature = "ispc_cmp")]
-    ispc_compile::Config::new()
+        // compilation fails.
+
+        let target_os = if cfg!(target_os = "windows") {
+            TargetOS::Windows
+        } else if cfg!(target_os = "linux") {
+            TargetOS::Linux
+        } else if cfg!(target_os = "macos") {
+            TargetOS::Macos
+        } else if cfg!(target_os = "android") {
+            TargetOS::Android
+        } else {
+            panic!("Unsupported platform")
+        };
+
+
+        ispc_compile::Config::new()
         .file("src/ispc/kernels/lanczos3.ispc")
         .opt_level(2)
         .woff()
@@ -17,12 +30,18 @@ fn main() {
             TargetISA::AVX512KNLi32x16,
             TargetISA::AVX512SKXi32x16,
         ])
-        .target_os(TargetOS::Windows)
+        .target_os(target_os)
         .out_dir("src/ispc")
         .compile("downsample_ispc");
-    if !cfg!(feature = "ispc_cmp") {
-        ispc_rt::PackagedModule::new("kernel")
+}
+
+#[cfg(not(feature = "ispc_cmp"))]
+fn compile_bindings() {
+    ispc_rt::PackagedModule::new("downsample_ispc")
             .lib_path("src/ispc")
             .link();
-    }
+}
+
+fn main() {
+    compile_bindings();
 }
