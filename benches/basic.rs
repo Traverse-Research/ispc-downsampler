a1bbc859bf03d02ce109c125ca9ac1c3874ac71e
Jasper Bekkers <bekkers@gmail.com>
2022-04-23 12:24:32 +0200

    Add benchmark for `resize` crate

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,9 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use ispc_downsampler::{downsample, Format, Image};
+use resize::{px::RGB, Type::Lanczos3};
 use stb_image::image::{load, LoadResult};
-use ispc_downsampler::{Image, downsample, Format};
 use std::path::Path;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn ispc_downsampler(c: &mut Criterion) {
     if let LoadResult::ImageU8(img) = load(Path::new("test_assets/square_test.png")) {
         let src_fmt = if img.data.len() / (img.width * img.height) == 4 {
             Format::RGBA8
@@ -11,18 +12,49 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             Format::RGB8
         };
 
-        println!("Loaded image!");
-
         let src_img = Image::new(&img.data, img.width as u32, img.height as u32, src_fmt);
 
         let target_width = (img.width / 4) as u32;
         let target_height = (img.height / 4) as u32;
 
-        c.bench_function("Downsample `square_test.png`", |b| b.iter(||
-            downsample(&src_img, target_width, target_height)
-        ));
+        c.bench_function("Downsample `square_test.png` using ispc_downsampler", |b| {
+            b.iter(|| downsample(&src_img, target_width, target_height))
+        });
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn resize_rs(c: &mut Criterion) {
+    if let LoadResult::ImageU8(img) = load(Path::new("test_assets/square_test.png")) {
+        let src_fmt = if img.data.len() / (img.width * img.height) == 4 {
+            Format::RGBA8
+        } else {
+            Format::RGB8
+        };
+
+        let src_img = Image::new(&img.data, img.width as u32, img.height as u32, src_fmt);
+
+        let target_width = img.width / 4;
+        let target_height = img.height / 4;
+
+        let src = vec![RGB::new(0, 0, 0); img.width * img.height];
+        let mut dst = vec![RGB::new(0, 0, 0); target_width * target_height];
+
+        c.bench_function("Downsample `square_test.png` using resize", |b| {
+            b.iter(|| {
+                let mut resizer = resize::new(
+                    img.width,
+                    img.height,
+                    target_width,
+                    target_height,
+                    resize::Pixel::RGB8,
+                    Lanczos3,
+                )
+                .unwrap();
+                resizer.resize(&src, &mut dst)
+            })
+        });
+    }
+}
+
+criterion_group!(benches, ispc_downsampler, resize_rs);
 criterion_main!(benches);
