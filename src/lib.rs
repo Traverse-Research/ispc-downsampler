2029fd83c51ace0c5278d0c0119cd5171f76e247
Jasper Bekkers <bekkers@gmail.com>
2022-04-29 23:36:42 +0200

    Test to hoist out the byte to float conversion

--- src/lib.rs
+++ src/lib.rs
@@ -36,12 +36,25 @@ pub fn downsample(src: &Image, target_width: u32, target_height: u32) -> Vec<u8>
     assert!(src.width >= target_width, "The width of the source image is less than the target's width. You are trying to upsample rather than downsample");
     assert!(src.height >= target_height, "The width of the source image is less than the target's width. You are trying to upsample rather than downsample");
 
-    let mut output = Vec::new();
-    // TODO: This and the kernel both assume RGBA8 textures. This will crash and burn with RGB8
-    output.resize(
-        (target_width * target_height * src.format.num_channels() as u32) as usize,
-        0,
-    );
+    let output_size = (target_width * target_height * src.format.num_channels() as u32) as usize;
+    let mut output : Vec<u8> = Vec::with_capacity(output_size);
+    unsafe { output.set_len(output_size); }
+
+    // intermediate upscale to f32's so we only have to do the conversion once
+    let intermediate_size = (src.width * src.height * src.format.num_channels() as u32) as usize;
+    let mut intermediate: Vec<f32> = Vec::with_capacity(intermediate_size);
+    unsafe { intermediate.set_len(intermediate_size); }
+
+
+    unsafe {
+        ispc::downsample_ispc::convert_f32(
+            src.width,
+            src.height,
+            src.format.num_channels(),
+            src.pixels.as_ptr(),
+            intermediate.as_mut_ptr(),
+        )
+    }
 
     unsafe {
         ispc::downsample_ispc::resample(
@@ -51,7 +64,7 @@ pub fn downsample(src: &Image, target_width: u32, target_height: u32) -> Vec<u8>
             src.format.num_channels(),
             target_width,
             target_height,
-            src.pixels.as_ptr(),
+            intermediate.as_ptr(),
             output.as_mut_ptr(),
         )
     }
