3e87b963e4b4918e0cadc33b725c1dec7d486f60
Marijn Suijten <marijn@traverseresearch.nl>
2022-07-13 12:49:02 +0200

    Add aarch64 prebuilts (#5)
    * ci: Build binaries for Linux and Android on aarch64
    
    * ci: Also generate aarch64 MacOS/iOS binaries
    
    * build: Use neon ISA for arm/aarch64
    
    * ci: Add GCC multilib for missing headers
    
    * Use `uint32` over `uint` in `export` symbol to be extra cautious
    
    * Add updated aarch64 binaries from CI
    
    On commit 975d9f1852719c411fd44f0ca61185c505a72116:
    https://github.com/Traverse-Research/ispc-downsampler/actions/runs/2662475612

--- build.rs
+++ build.rs
@@ -5,18 +5,28 @@ fn compile_bindings() {
     // Compile our ISPC library, this call will exit with EXIT_FAILURE if
     // compilation fails.
 
-    Config::new()
-        .file("src/ispc/kernels/lanczos3.ispc")
-        .opt_level(2)
-        .woff()
-        .target_isas(vec![
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_isas = match target_arch.as_str() {
+        "x86" | "x86_64" => vec![
             TargetISA::SSE2i32x4,
             TargetISA::SSE4i32x4,
             TargetISA::AVX1i32x8,
             TargetISA::AVX2i32x8,
             TargetISA::AVX512KNLi32x16,
             TargetISA::AVX512SKXi32x16,
-        ])
+        ],
+        "arm" | "aarch64" => vec![
+            // TargetISA::Neoni32x4,
+            TargetISA::Neoni32x8,
+        ],
+        x => panic!("Unsupported target architecture {}", x),
+    };
+
+    Config::new()
+        .file("src/ispc/kernels/lanczos3.ispc")
+        .opt_level(2)
+        .woff()
+        .target_isas(target_isas)
         .math_lib(MathLib::Fast)
         .bindgen_options(BindgenOptions {
             allowlist_functions: vec!["resample".into()],
